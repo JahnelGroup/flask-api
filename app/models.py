4ed9e7af2340c5fad80cb1a82c1c1175fb767862
Steven Zgaljic <szgaljic@gmail.com>
2019-07-10 12:30:45 -0500

    added BaseModel

--- app/models.py
+++ app/models.py
@@ -2,7 +2,14 @@ from app import db, ma
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
-class User(db.Model):
+class BaseModel(db.Model):
+    __abstract__ = True
+
+    created_on = db.Column(db.DateTime, default=db.func.now())
+    updated_on = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+
+
+class User(BaseModel):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
